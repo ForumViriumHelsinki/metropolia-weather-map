bb0fa20bd67d0db6325788e387e76c0015754a04
JoonasMV <JoonasMRViljanen@gmail.com>
2025-02-12 11:16:04 +0200

    Add animation to SensorInfo divs when expanding

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
 import SensorInfo from "@/components/SensorInfo";
-import TestMap from "@/components/TestMap";
 import tempData from "./temp-data.json";
 
 export type TempData = {
@@ -15,22 +14,26 @@ export default async function Home() {
   return (
     <main className="flex flex-col gap-12">
       {/* Hero */}
-      <div className="dark:bg-dark-banner flex w-full justify-between bg-yellow-pale px-[16.666%]">
+      <div className="flex w-full justify-between bg-yellow-pale px-[16.666%] dark:bg-dark-banner">
         <h1 className="w-1/4 pt-9 text-5xl">Mäkelänkatu</h1>
-        <div className="aspect-[2/1] w-1/2 border-2">
-          <TestMap />
-        </div>
+        <div className="aspect-[2/1] w-1/2 border-2">{/* <TestMap /> */}</div>
       </div>
 
       {/* Sensor data */}
       <div className="mx-auto w-4/6">
         <h2 className="mb-4 block text-4xl">Sensorit</h2>
-        <div className="z-auto grid grid-cols-5 gap-6">
+        <div className="grid grid-cols-5 gap-6">
           {tData.map((d, index) => (
-            <SensorInfo
+            <div
               key={index}
-              data={d}
-            />
+              // Dynamically created Tailwind classes don't work here
+              style={{
+                position: "relative",
+                zIndex: -Math.floor(index / 5) + 999,
+              }}
+            >
+              <SensorInfo data={d} />
+            </div>
           ))}
         </div>
       </div>
--- src/components/SensorInfo.tsx
+++ src/components/SensorInfo.tsx
@@ -5,37 +5,36 @@ import { useState } from "react";
 
 const SensorInfo = ({ data }: { data: TempData }) => {
   const [visible, setVisible] = useState<boolean>(false);
-  const bgColor = visible ? "bg-red-300" : "bg-yellow";
+  const bgColor = visible ? "bg-[#F2BB0A]" : "bg-yellow";
 
   return (
-    <div className="relative [&_h4]:text-xl [&_h4]:font-roman">
+    <div
+      className="relative [&>div]:px-6 [&>div]:duration-500 [&>div]:ease-linear [&_h4]:text-xl [&_h4]:font-roman"
+      onClick={() => setVisible((v) => !v)}
+    >
       <div
-        className={`${bgColor} ${!visible && "pb-2"} px-6 pt-2 text-black`}
-        onClick={() => setVisible((v) => !v)}
+        className={`${bgColor} ${visible ? "sensor-info-shadow" : ""} relative h-fit py-2 text-black transition-all`}
       >
-        <div className="">
-          <h3 className="text-2xl font-medium">Sensori</h3>
-          <div className="grid grid-cols-2 text-xl">
-            <span>{data.temperature}°C</span>
-            <span>{data.humidity}%</span>
-          </div>
+        <h3 className="text-2xl font-medium">Sensori</h3>
+        <div className="grid grid-cols-2 text-xl">
+          <span>{data.temperature}°C</span>
+          <span>{data.humidity}%</span>
         </div>
       </div>
 
-      {visible && (
-        <div
-          className={`${bgColor} absolute z-20 w-full px-6 pb-2 text-black [&_span]:text-lg`}
-        >
-          <div>
-            <h4>Sijainti</h4>
-            <span>{data.type}</span>
-          </div>
-          <div>
-            <h4>Kiinnitys</h4>
-            <span>{data.attached}</span>
-          </div>
+      {/* Details */}
+      <div
+        className={`${bgColor} absolute top-[calc(100%-8px)] h-fit overflow-hidden transition-all ${visible ? "sensor-info-shadow max-h-[150px] pb-2" : "max-h-0 pb-0"} w-full text-black [&_span]:text-lg`}
+      >
+        <div>
+          <h4>Sijainti</h4>
+          <span>{data.type}</span>
         </div>
-      )}
+        <div>
+          <h4>Kiinnitys</h4>
+          <span>{data.attached}</span>
+        </div>
+      </div>
     </div>
   );
 };
