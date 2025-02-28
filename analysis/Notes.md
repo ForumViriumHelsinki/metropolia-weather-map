a640b6e17195cf8573ea5c834286f98321c82a1d
JoonasMV <JoonasMRViljanen@gmail.com>
2025-02-28 14:25:51 +0200

    Refactor file structure

---
+++ analysis/Notes.md
@@ -0,0 +1,15 @@
+# Analysis
+
+## Notes
+
+Sensors stored until 27/5/24
+
+- Remove from dataset
+
+daylightHours.csv
+
+- Data on 2/6 and 26/5 filled with the previous day's data
+
+## To analyze
+
+- Daily difference between sensors in shade and sun during day
---
+++ analysis/utils_new.py
@@ -0,0 +1,55 @@
+import pandas as pd
+
+SENSORS = [
+    "24E124136E106616",
+    "24E124136E106617",
+    "24E124136E106618",
+    "24E124136E106619",
+    "24E124136E106635",
+    "24E124136E106636",
+    "24E124136E106637",
+    "24E124136E106638",
+    "24E124136E106643",
+    "24E124136E106661",
+    "24E124136E106674",
+    "24E124136E106686",
+]
+
+SENSOR_SUN = [
+    "24E124136E106637",
+    "24E124136E106638",
+    "24E124136E106619",
+    "24E124136E106661",
+]
+
+SENSOR_SHADE = [
+    "24E124136E106616",
+    "24E124136E106617",
+    "24E124136E106618",
+    "24E124136E106635",
+    "24E124136E106636",
+    "24E124136E106643",
+    "24E124136E106674",
+    "24E124136E106686",
+]
+
+
+def get_csv():
+    df = pd.read_csv("../data/makelankatu-2024.csv")
+    df["time"] = pd.to_datetime(df["time"])
+
+    return df
+
+
+def separate_sensors(sensor_df):
+    filtered = {
+        sensor_id: group
+        for sensor_id, group in sensor_df.groupby("dev-id")
+        if sensor_id in SENSORS
+    }
+    return filtered
+
+
+def apply_date_range(df, start_date, end_date):
+    mask = (df["time"] >= start_date) & (df["time"] <= end_date)
+    return df.loc[mask]
