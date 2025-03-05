05bb9004058f36849b5dc34ea85ef715eb0ebdd3
JoonasMV <JoonasMRViljanen@gmail.com>
2025-03-05 11:43:49 +0200

    Add default env variables

--- scripts/load_sensors.py
+++ scripts/load_sensors.py
@@ -15,11 +15,11 @@ FOLDER_PATH = os.path.abspath(
 
 TABLE = "weather.sensors"
 
-DB_NAME = os.getenv("DB_NAME")
-DB_HOST = os.getenv("DB_HOST")
-DB_PORT = os.getenv("DB_PORT")
-DB_USER = os.getenv("DB_USER")
-DB_PASS = os.getenv("DB_PASS")
+DB_NAME = os.getenv("DB_NAME", "weatherdb")
+DB_HOST = os.getenv("DB_HOST", "localhost")
+DB_PORT = os.getenv("DB_PORT", "5432")
+DB_USER = os.getenv("DB_USER", "postgres")
+DB_PASS = os.getenv("DB_PASS", "pass")
 
 print(DB_HOST)
 
