ee20dfd40b906ac9d29c49f101a71c2675f68d20
JoonasMV <JoonasMRViljanen@gmail.com>
2025-03-06 15:43:07 +0200

    Start creating parameterized fetch for sensors Remove unused files

--- client/src/types/index.ts
+++ client/src/types/index.ts
@@ -14,3 +14,14 @@ export type SensorData = {
   temperature: number;
   sensor: string;
 };
+
+export interface SensorServiceParams {
+  id?: string;
+  location?: [number, number];
+  type?: string;
+  note?: string;
+  attached?: string;
+  install_date_from?: string;
+  install_date_to?: string;
+  fields?: string[];
+}
