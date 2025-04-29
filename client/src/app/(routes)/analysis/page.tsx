f51ad32f8586852330599e961bcdad7fa327eec2
Joonas V <JoonasMRViljanen@gmail.com>
2025-04-29 08:32:05 +0300

    Add service to fetch graphs and form to get a graph

--- client/src/app/(routes)/analysis/page.tsx
+++ client/src/app/(routes)/analysis/page.tsx
@@ -1,28 +1,90 @@
 "use client";
 
-import { apiFetch } from "@/utils/apiFetch";
+import {
+  getTagGraphService,
+  TagGraphParams,
+} from "@/app/services/tags/getTagGraphService";
 import { useState } from "react";
 
 const Analysis = () => {
   const [graphUrl, setGraphUrl] = useState<string>("");
+  const [graphParams, setGraphParams] = useState<TagGraphParams>({
+    tag1: "",
+    tag2: "",
+    location: "",
+    graph_type: "",
+  });
 
   const getGraph = async () => {
-    const res = await apiFetch("/analysis/temperature");
-    const blob = await res.blob();
-    const graphUrl = URL.createObjectURL(blob);
-    setGraphUrl(graphUrl);
+    try {
+      const blob = await getTagGraphService({
+        tag1: "meri",
+        tag2: "manner",
+        location: "Vallila",
+        graph_type: "plot",
+      });
+      setGraphUrl(URL.createObjectURL(blob));
+    } catch (error) {
+      if (error instanceof Error) {
+        console.error(error);
+      }
+    }
   };
 
   return (
-    <div>
+    <div className="flex flex-col gap-4">
       <h1>Analysis</h1>
+
       <button
         className="box-basic"
         onClick={getGraph}
       >
         Get graph
       </button>
+
+      <form className="box-basic flex flex-col">
+        <label>Tag1</label>
+        <input
+          type="text"
+          value={graphParams.tag1}
+          onChange={(e) =>
+            setGraphParams({ ...graphParams, tag1: e.currentTarget.value })
+          }
+        />
+
+        <label>Tag2</label>
+        <input
+          type="text"
+          value={graphParams.tag2}
+          onChange={(e) =>
+            setGraphParams({ ...graphParams, tag2: e.currentTarget.value })
+          }
+        />
+
+        <label>Location</label>
+        <input
+          type="text"
+          value={graphParams.location}
+          onChange={(e) =>
+            setGraphParams({ ...graphParams, location: e.currentTarget.value })
+          }
+        />
+
+        <label>Graph type</label>
+        <input
+          type="text"
+          value={graphParams.graph_type}
+          onChange={(e) =>
+            setGraphParams({
+              ...graphParams,
+              graph_type: e.currentTarget.value,
+            })
+          }
+        />
+      </form>
+
       {graphUrl && (
+        // eslint-disable-next-line @next/next/no-img-element
         <img
           src={graphUrl}
           alt="Temperature Analysis Graph"
---
+++ client/src/app/services/tags/getTagGraphService.ts
@@ -0,0 +1,39 @@
+import { apiFetch } from "@/utils/apiFetch";
+
+export interface TagGraphParams {
+  tag1: string;
+  tag2: string;
+  location: string;
+  graph_type: string;
+  start_date?: string | null;
+  end_date?: string | null;
+  daytime?: boolean;
+  nighttime?: boolean;
+}
+
+export const getTagGraphService = async (
+  params: TagGraphParams,
+): Promise<Blob> => {
+  try {
+    const searchParams = new URLSearchParams();
+    for (const [key, val] of Object.entries(params)) {
+      searchParams.set(key, val);
+    }
+
+    const res = await apiFetch(
+      `/analysis/temperature?${searchParams.toString()}`,
+    );
+
+    if (!res.ok) {
+      const errorData = await res.json();
+      console.error(errorData);
+      throw new Error("Error creating data", errorData.detail);
+    }
+
+    const data = await res.blob();
+    return data;
+  } catch (error) {
+    console.error("Error fetching graph", error);
+    throw error;
+  }
+};
