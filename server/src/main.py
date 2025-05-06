404d14cf9ca8c53785036e2ec516ef669174fc25
Joonas V <JoonasMRViljanen@gmail.com>
2025-05-06 21:13:20 +0300

    Return daylight25.csv to fix analysis scripts, delete unused cache logic

--- server/src/main.py
+++ server/src/main.py
@@ -14,9 +14,6 @@ from api.routes.graph_routes import graph_router
 from api.routes.sensor_tags import sensor_tag_router
 from api.routes.sensors import sensor_router
 from api.routes.tags import tag_router
-from cache import DATA_CACHE
-
-# from utils.get_data_util import get_by_location
 
 logging.basicConfig(level=logging.INFO, format="%(message)s")
 logger = logging.getLogger(__name__)
@@ -24,7 +21,7 @@ logger = logging.getLogger(__name__)
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-	yield
+    yield
 
 
 app = FastAPI(lifespan=lifespan)
@@ -32,39 +29,37 @@ app = FastAPI(lifespan=lifespan)
 origins = ["http://localhost:3000"]
 
 app.add_middleware(
-	CORSMiddleware,
-	allow_origins=origins,
-	allow_credentials=True,
-	allow_methods=["*"],
-	allow_headers=["*"],
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 
 @app.middleware("http")
 async def log_request_info(request: Request, call_next):
-	logger.info("GET %s", request.url.path)
-	if request.query_params:
-		for k, v in request.query_params.items():
-			logger.info("%s - %s", k, v)
+    logger.info("GET %s", request.url.path)
+    if request.query_params:
+        for k, v in request.query_params.items():
+            logger.info("%s - %s", k, v)
 
-	return await call_next(request)
+    return await call_next(request)
 
 
 @app.get("/api/test")
 def test():
-	print(DATA_CACHE.head())
-	# get_by_location()
-	return {"Hello"}
+    return {"Hello"}
 
 
 @app.get("/api")
 def home():
-	return {"This is the api home"}
+    return {"This is the api home"}
 
 
 @app.get("/update")
 def update_hello():
-	return {"message": "Global variable updated"}
+    return {"message": "Global variable updated"}
 
 
 app.include_router(sensor_router)
